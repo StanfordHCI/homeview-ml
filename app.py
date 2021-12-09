4bc11ec14bf6ef4b246d4db58307b893b283eac2
Zhouyue_Lyu <zhuoyue.lyu@gmail.com>
2021-12-09 08:27:34 -0500

    update

--- app.py
+++ app.py
@@ -1,54 +1,55 @@
-# from train import Model
-# import torch
+from train import Model
+import torch
 
-# import numpy as np
-# import sys
-#
-# import glob
-# import os
-#
-# import open3d
+import numpy as np
+import sys
+
+import glob
+import os
+
+import open3d
 
 import zmq
-# import json
+
+import json
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind('tcp://*:5556')
 
-# # load vecs
-# # dataset_name = 'vh.' + sys.argv[1]
-# dataset_name = "vh.cameras"
-# train_data = torch.load(dataset_name + '/train.pth')
-#
-# # (n_chunks, n_frames) distances
-# vecs = np.array([data[1].tolist() for data in train_data]).transpose()
-#
-# # instantiate model
-# n_sensors = train_data[0][0].shape[0]
-# n_chunks = train_data[0][1].shape[0]
-# model = Model(n_sensors, n_chunks)
-#
-# # load model
-# ckpt = torch.load(dataset_name + '/.pth')
-# model.load_state_dict(ckpt)
-# model.eval()
-#
-#
-# def get_frame_ids(sensors):
-#     pred_vecs = model(sensors).detach().numpy()
-#     pred_frame_ids = [int(np.argmin(abs(vec - pred_vec))) \
-#                       for vec, pred_vec in zip(vecs, pred_vecs)]
-#     return pred_frame_ids
-#
-#
-# prev_frame_ids = [-1] * n_chunks
-# save_directory = dataset_name + '/tmp'
-#
-# if not os.path.exists(save_directory):
-#     os.makedirs(save_directory)
-#
-# sensors = [sensor['state'] for sensor in json.load(open('./0.json'))]
+# load vecs
+# dataset_name = 'vh.' + sys.argv[1]
+dataset_name = "vh.cameras"
+train_data = torch.load(dataset_name + '/train.pth')
+
+# (n_chunks, n_frames) distances
+vecs = np.array([data[1].tolist() for data in train_data]).transpose()
+
+# instantiate model
+n_sensors = train_data[0][0].shape[0]
+n_chunks = train_data[0][1].shape[0]
+model = Model(n_sensors, n_chunks)
+
+# load model
+ckpt = torch.load(dataset_name + '/t.pth')
+model.load_state_dict(ckpt)
+model.eval()
+
+
+def get_frame_ids(sensors):
+    pred_vecs = model(sensors).detach().numpy()
+    pred_frame_ids = [int(np.argmin(abs(vec - pred_vec))) \
+                      for vec, pred_vec in zip(vecs, pred_vecs)]
+    return pred_frame_ids
+
+
+prev_frame_ids = [-1] * n_chunks
+save_directory = dataset_name + '/tmp'
+
+if not os.path.exists(save_directory):
+    os.makedirs(save_directory)
+
+sensors = [sensor['state'] for sensor in json.load(open('./99.json'))][0:88]
 
 while True:
     rec_sensors = socket.recv()
@@ -56,6 +57,7 @@ while True:
     rec_sensors = rec_sensors.decode('ascii')
     print(rec_sensors)
     import time
+
     time.sleep(1)
     if rec_sensors == 'S':
         socket.send(b"Nothing")
@@ -69,27 +71,27 @@ while True:
                              44,
                              62,
                              75]
-        # for i in range(len(rec_sensors)):
-        #     sensors[light_sensors_idx[i] - 1] = rec_sensors[i]
-        # sensors = torch.tensor(sensors, dtype=torch.float32)
+        for i in range(len(rec_sensors)):
+            sensors[light_sensors_idx[i] - 1] = rec_sensors[i]
+        sensors = torch.tensor(sensors, dtype=torch.float32)
 
-        # pred_frame_ids = get_frame_ids(sensors)
-        #
-        # files = glob.glob(save_directory + '/*')
-        # for file in files:
-        #     os.remove(file)
+        pred_frame_ids = get_frame_ids(sensors)
 
-        # for chunk_id, (prev_frame_id, pred_frame_id) in enumerate(zip(prev_frame_ids, pred_frame_ids)):
-        #     if pred_frame_id != prev_frame_id:
-        #         chunk_points = np.load(dataset_name + '/chunk/%d-%d.npz' % (pred_frame_id, chunk_id))['arr_0']
-        #         chunk_cloud = open3d.geometry.PointCloud()
-        #         chunk_cloud.points = open3d.utility.Vector3dVector(chunk_points[:, :3])
-        #         chunk_cloud.colors = open3d.utility.Vector3dVector(chunk_points[:, 3:])
-        #         open3d.io.write_point_cloud('%s/%d.ply' % (save_directory, chunk_id), chunk_cloud)
+        files = glob.glob(save_directory + '/*')
+        for file in files:
+            os.remove(file)
 
+        for chunk_id, (prev_frame_id, pred_frame_id) in enumerate(zip(prev_frame_ids, pred_frame_ids)):
+            if pred_frame_id != prev_frame_id:
+                chunk_points = np.load(dataset_name + '/chunk/%d-%d.npz' % (pred_frame_id, chunk_id))['arr_0']
+                chunk_cloud = open3d.geometry.PointCloud()
+                chunk_cloud.points = open3d.utility.Vector3dVector(chunk_points[:, :3])
+                chunk_cloud.colors = open3d.utility.Vector3dVector(chunk_points[:, 3:])
+                address_zy = '/Users/zhuoyuelyu/Documents/a-Stanford/StanfordHCI/virtualhome-11/Assets/ply-common'
+                open3d.io.write_point_cloud('%s/%d.ply' % (address_zy, chunk_id), chunk_cloud)
 
-        # prev_frame_ids = pred_frame_ids
-        prev_frame_ids = [1, 2, 3]
+        prev_frame_ids = pred_frame_ids
+        # prev_frame_ids = [1, 2, 3]
         # convert it into string
         prev_frame_ids = '_'.join(str(id) for id in prev_frame_ids)
         socket.send_string(prev_frame_ids)
---
+++ test-new.py
@@ -0,0 +1,132 @@
+from utils import get_cloud, get_chunks, get_coords_and_colors, merge_clouds, fn
+from train import Model
+from glob import glob
+import numpy as np
+import config
+import open3d
+import torch
+import time
+import sys
+
+### Step 1: read data, get data file list
+
+# dataset_name = 'vh.' + sys.argv[1]
+dataset_name = "vh.cameras"
+# if config.n_cameras exceeds 100, modify fn in utils.py
+
+coord_files = sorted(glob(dataset_name + '/raw/*.exr'), key=fn)
+n_frames = len(coord_files) // config.n_cameras
+assert n_frames > config.n_train
+
+color_files = sorted(glob(dataset_name + '/raw/*.png'), key=fn)
+assert n_frames == len(color_files) // config.n_cameras
+
+json_files = sorted(glob(dataset_name + '/raw/*.json'), key=fn)
+json_files = json_files[0:n_frames]
+# assert n_frames == len(json_files)
+
+
+### Step 2: process data
+
+train_data = torch.load(dataset_name + '/old-pth/train.pth')
+eval_data = torch.load(dataset_name + '/old-pth/eval.pth')
+
+# (n_chunks, n_frames) distances
+vecs = np.array([data[1].tolist() for data in train_data]).transpose()
+
+# bounding boxes
+# coords, _ = get_coords_and_colors(coord_files[:config.n_cameras], coord_files[:config.n_cameras])
+# chunks = get_chunks(coords, config.chunk_size)
+
+# frame_clouds = []
+
+# for frame_id in range(n_frames):
+#   coords, colors = get_coords_and_colors(
+#     coord_files[frame_id * config.n_cameras:(frame_id + 1) * config.n_cameras],
+#     color_files[frame_id * config.n_cameras:(frame_id + 1) * config.n_cameras]
+#   )
+#   frame_clouds.append(get_cloud(coords, colors))
+# frame_clouds[frame_id] = get_cloud(coords, colors)
+
+
+### Step 3: setup model
+
+n_sensors = train_data[0][0].shape[0]
+n_chunks = train_data[0][1].shape[0]
+model = Model(n_sensors, n_chunks)
+
+# ckpt = torch.load(dataset_name + '/pth')
+# model.load_state_dict(ckpt)
+# model.eval()
+
+### Step 4: evaluation
+
+# test_id = int(sys.argv[2])
+test_id = 0
+sensors = eval_data[test_id - config.n_train][0] \
+    if test_id > config.n_train else train_data[test_id][0]
+
+pred_vecs = model(sensors).detach().numpy()
+
+### Step 5: assemble, visualize
+
+assemble_start = time.time()
+print('assemble start')
+
+frame_clouds = {}
+chunk_clouds = []
+
+assemble_time = 0.0
+
+for chunk_id, (vec, pred_vec, chunk) in enumerate(zip(vecs, pred_vecs, chunks)):
+    # find closest distance (most matching frame) from database
+    frame_id = np.argmin(abs(vec - pred_vec))
+    print(frame_id)
+
+    if frame_id not in frame_clouds:
+        coords, colors = get_coords_and_colors(
+            coord_files[frame_id * config.n_cameras:(frame_id + 1) * config.n_cameras],
+            color_files[frame_id * config.n_cameras:(frame_id + 1) * config.n_cameras]
+        )
+        frame_clouds[frame_id] = get_cloud(coords, colors)
+
+    start = time.time()
+
+    chunk_cloud = frame_clouds[frame_id].crop(chunk)
+    chunk_clouds.append(chunk_cloud)
+
+    assemble_time += time.time() - start
+
+    # DEBUG
+    # if chunk_id == 51:
+    #   print(pred_vec, frame_id)
+    #   print(vec)
+    #   open3d.visualization.draw_geometries([chunk_clouds[frame_id].crop(chunk)])
+
+assemble_end = time.time()
+print(assemble_end - assemble_start, assemble_time)
+
+### Step 6: remove ceiling
+
+coords, colors = get_coords_and_colors(
+    coord_files[test_id * config.n_cameras:(test_id + 1) * config.n_cameras],
+    color_files[test_id * config.n_cameras:(test_id + 1) * config.n_cameras]
+)
+ground_truth = get_cloud(coords, colors)
+
+ceiling = ground_truth.get_axis_aligned_bounding_box()
+
+no_ceiling = open3d.geometry.AxisAlignedBoundingBox(
+    np.array(ceiling.min_bound),
+    np.array([ceiling.max_bound[0], ceiling.max_bound[1] - 1e-1, ceiling.max_bound[2]])
+)  # any more elegant way to 'crop' a bounding box?
+
+ground_truth = ground_truth.crop(no_ceiling)
+chunk_clouds = [cloud.crop(no_ceiling) for cloud in chunk_clouds]
+
+open3d.visualization.draw_geometries([ground_truth])
+
+chunk_clouds = merge_clouds(chunk_clouds)
+open3d.visualization.draw_geometries([chunk_clouds])
+
+open3d.io.write_point_cloud('01.cameras.ply', chunk_clouds)
